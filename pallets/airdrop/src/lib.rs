2a29bee71c455f358a3d7befd00db5c9fb325ca7
Krayt78 <ludovic.domingues96@gmail.com>
2025-01-28 16:32:49 +0100

    added eth address serialisation and fmt

--- pallets/airdrop/src/lib.rs
+++ pallets/airdrop/src/lib.rs
@@ -10,13 +10,56 @@ use polkadot_sdk::polkadot_sdk_frame as frame;
 // Re-export all pallet parts, this is needed to properly import the pallet into the runtime.
 pub use pallet::*;
 
+use crate::frame::derive::{RuntimeDebug, TypeInfo};
+use codec::{Decode, Encode, MaxEncodedLen};
+use serde::{self, Deserialize, Deserializer, Serialize, Serializer};
+
+/// An Ethereum address (i.e. 20 bytes, used to represent an Ethereum account).
+///
+/// This gets serialized to the 0x-prefixed hex representation.
+#[derive(
+    Clone, Copy, PartialEq, Eq, Encode, Decode, Default, RuntimeDebug, TypeInfo, MaxEncodedLen,
+)]
+pub struct EthereumAddress([u8; 20]);
+
+impl Serialize for EthereumAddress {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let hex: String = rustc_hex::ToHex::to_hex(&self.0[..]);
+        serializer.serialize_str(&format!("0x{}", hex))
+    }
+}
+
+impl<'de> Deserialize<'de> for EthereumAddress {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let base_string = String::deserialize(deserializer)?;
+        let offset = if base_string.starts_with("0x") { 2 } else { 0 };
+        let s = &base_string[offset..];
+        if s.len() != 40 {
+            Err(serde::de::Error::custom(
+                "Bad length of Ethereum address (should be 42 including '0x')",
+            ))?;
+        }
+        let raw: Vec<u8> = rustc_hex::FromHex::from_hex(s)
+            .map_err(|e| serde::de::Error::custom(format!("{:?}", e)))?;
+        let mut r = Self::default();
+        r.0.copy_from_slice(&raw);
+        Ok(r)
+    }
+}
+
 #[frame::pallet]
 pub mod pallet {
-	use super::*;
+    use super::*;
 
-	#[pallet::config]
-	pub trait Config: polkadot_sdk::frame_system::Config {}
+    #[pallet::config]
+    pub trait Config: polkadot_sdk::frame_system::Config {}
 
-	#[pallet::pallet]
-	pub struct Pallet<T>(_);
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
 }
